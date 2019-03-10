eba9b7dc458ae778825b304e62ba38f14b532892
Rhonda <rtinkham0408@gmail.com>
2019-03-09 23:50:48 -0500

    added authentication, login page

---
+++ client/src/components/Auth/Login.js
@@ -0,0 +1,62 @@
+import React, { useContext } from "react";
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from "@material-ui/core/styles";
+import Context from '../../context';
+import Typography from "@material-ui/core/Typography";
+import {ME_QUERY} from '../../graphql/queries';
+
+const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context)
+
+  const onSuccess = async googleUser => {
+    try {
+      const idToken = googleUser.getAuthResponse().id_token
+      const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: { authorization: idToken }
+      });
+      const { me } = await client.request(ME_QUERY);
+      // console.log(me);
+      dispatch({ type: "LOGIN_USER", payload: me });
+    } catch (err) {
+      onFailure(err);
+    }
+  };
+
+  const onFailure = err => {
+    console.error('Error logging in', err);
+  }
+
+  return (
+    <div className={classes.root}>
+      <Typography
+        component='h1'
+        variant='h3'
+        gutterBottom
+        noWrap
+        style={{ color: "rgb(66, 133, 244)"}}
+      >
+        Welcome
+      </Typography>
+      <GoogleLogin   
+        clientId="342073331731-3dgeo3f55loial8qcdldrtpdqu00ecrh.apps.googleusercontent.com" onSuccess={onSuccess}
+        onFailure={onFailure}
+        isSignedIn={true}
+        theme='dark'
+      />
+    </div>
+    
+  );
+};
+
+const styles = {
+  root: {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center"
+  }
+};
+
+export default withStyles(styles)(Login);
---
+++ client/src/withRoot.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import indigo from "@material-ui/core/colors/indigo";
+import deepOrange from "@material-ui/core/colors/deepOrange";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+// A theme with custom primary and secondary color.
+// It's optional.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: indigo[300],
+      main: indigo[500],
+      dark: indigo[700]
+    },
+    secondary: {
+      light: deepOrange[300],
+      main: deepOrange[500],
+      dark: deepOrange[700]
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
+function withRoot(Component) {
+  function WithRoot(props) {
+    // MuiThemeProvider makes the theme available down the React tree
+    // thanks to React context.
+    return (
+      <MuiThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        {/* https://material-ui.com/getting-started/usage/#cssbaseline */}
+        <CssBaseline />
+        <Component {...props} />
+      </MuiThemeProvider>
+    );
+  }
+
+  return WithRoot;
+}
+
+export default withRoot;
